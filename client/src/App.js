5abceca9bb4ed431e4e699459bc8eef9ec908429
Indasia Summerfield <Indasia@yahoo.com>
2019-07-02 11:09:38 -0700

    create components, app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+// import React
+import React, { Component } from 'react';
 import './App.css';
+import Courses from './components/Courses';
+// import './App.css';
+import {
+  BrowserRouter,
+  // Route
+} from 'react-router-dom';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  // store the output from API request
+  state = {
+    courses: []
+  };
+
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/')
+    // request API
+    axios.get('http://localhost:5000/api/courses')
+      // parse output to JSON
+      .then(res => res.json())
+      // set value of the state to the output from API
+      .then((data) => {
+        this.setState({ courses: data })
+      })
+      // log errors to console
+      .catch(console.log)
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Courses courses={this.state.courses} />
+        </div>
+      </BrowserRouter>
+    )
+  }
+};
 
 export default App;
