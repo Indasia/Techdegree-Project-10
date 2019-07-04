5b4a6d4dd8f43bcfa155d3a9492e7e961cdad776
Indasia Summerfield <Indasia@yahoo.com>
2019-07-04 10:03:18 -0700

    coding Header.js

---
+++ client/src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return localStorage.getItem('id') ? (
+        <div className='header'>
+            <div className='bounds'>
+                <h1 className='header--logo'>Courses</h1>
+                <nav>
+                    <span>Welcome {localStorage.getItem('firstName')} {localStorage.getItem('lastName')}</span>
+                    <Link className='signout' to='/signout'> Sign Out </Link>
+                </nav>
+            </div>
+        </div>
+    ) : (
+            <div className='header'>
+                <div className='bounds'>
+                    <h1 className='header--logo'>Courses</h1>
+                    <nav>
+                        <Link className='signup' to={'/UserSignIn'}> Sign Up </Link>
+                        <Link className='signin' to={'/UserSignUp'}> Sign In </Link>
+                    </nav>
+                </div>
+            </div>
+    );
+};
+
+export default Header;
