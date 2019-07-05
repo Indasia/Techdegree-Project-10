7a9a65323e3515c971c0d38dac44a8dbe2e631a9
Indasia Summerfield <Indasia@yahoo.com>
2019-07-05 01:36:56 -0700

    fixed links in header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 const Header = () => {
     return localStorage.getItem('id') ? (
@@ -15,10 +15,12 @@ const Header = () => {
     ) : (
             <div className='header'>
                 <div className='bounds'>
-                    <h1 className='header--logo'>Courses</h1>
+                    <h1 className='header--logo'>
+                        <NavLink to='/courses'>Courses</NavLink>
+                    </h1>
                     <nav>
-                        <Link className='signup' to={'/UserSignIn'}> Sign Up </Link>
-                        <Link className='signin' to={'/UserSignUp'}> Sign In </Link>
+                        <Link className='signup' to={'/UserSignUp'}> Sign Up </Link>
+                        <Link className='signin' to={'/UserSignIn'}> Sign In </Link>
                     </nav>
                 </div>
             </div>
