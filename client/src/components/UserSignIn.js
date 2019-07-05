f3c81e02bfaa502f4407fa945a8004e9d05810e5
Indasia Summerfield <Indasia@yahoo.com>
2019-07-04 20:00:27 -0700

    worked on user sign in component

---
+++ client/src/components/UserSignIn.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class UserSignIn extends Component {
+
+    state = {
+        // set state (empty strings)
+        emailAddress: '',
+        password: ''
+    }
+
+    // as the value of input changes, set state value
+    handleInputChange = e => {
+        // prevent default
+        e.preventDefault();
+        // use name ref and field value
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    render() {
+        return (
+            <div className="bounds">
+                <div className="grid-33 centered signin">
+                    <h1>Sign In</h1>
+                    <div>
+                        {errorMessage ? (
+                            <div>
+                                <h2 className="validation--errors--label">Oops!</h2>
+                                <div className="validation-errors">
+                                    <ul>
+                                        <li>{errorMessage}</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        ): ''}
+                        <form onSubmit={e => this.props.signIn(e, this.state.emailAddress, this.state.password)}>
+                            <div>
+                                {/* email address input */}
+                                <input id="emailAddress" name="emailAddress" type="text" className="" placeholder="Email Address" onChange={this.handleInputChange} />
+                            </div>
+                            <div>
+                                {/* password input */}
+                                <input id="password" name="password" type="password" className="" placeholder="Password" onChange={this.handleInputChange} />
+                            </div>
+                            <div className="grid-100 pad-bottom">
+                                {/* sign in button */}
+                                <button className="button" type="submit">Sign In</button>
+                                {/* cancel button which redirects to courses */}
+                                <Link className="button button-secondary" to="/courses" >Cancel</Link>
+                            </div>
+                        </form>
+                    </div>
+                    {/*Reroute to home, conditionally */}
+                    {this.props.isAuthenticated === true && (this.props.history.goBack())}
+                    <p>&nbsp;</p>
+                    <p>Don't have a user account?
+                    <Link to="/signup"> Click here </Link>
+                    to sign up!</p>
+                </div>
+            </div>
+        );
+    };
+}
+
+
+export default UserSignIn;
