a78fc0e1993457306906157031c19d07e69bccbd
Indasia Summerfield <Indasia@yahoo.com>
2019-07-06 12:04:19 -0700

    attempting to make things work?

--- client/src/App.js
+++ client/src/App.js
@@ -29,51 +29,63 @@ class App extends Component {
     this.handleSignIn = this.handleSignIn.bind(this);
   }
    
+
   // handle sign in
   handleSignIn(userInfo) {
     // request user info
     axios.get('http://localhost:5000/api/users', {
+      // set authorization header
       auth: {
         username: userInfo.emailAddress,
         password: userInfo.password
       }
     }).then(res => {
-      console.log(res.data)
-      // save user preferences locally
-      window.localStorage.setItem('FirstName', res.data.firstName)
-      window.localStorage.setItem('LastName', res.data.lastName)
-      window.localStorage.setItem('Email', userInfo.emailAddress)
-      window.localStorage.setItem('Password', userInfo.password)
-      window.localStorage.setItem('UserId', JSON.stringify(res.data._id))
-      window.localStorage.setItem('IsLoggedIn', JSON.stringify(true))
-      window.location.assign('/')
+        // if request succeeds
+        if (res.status === 200) {
+          // save user preferences locally
+          window.localStorage.setItem('FirstName', res.data.firstName)
+          window.localStorage.setItem('LastName', res.data.lastName)
+          window.localStorage.setItem('Email', userInfo.emailAddress)
+          window.localStorage.setItem('Password', userInfo.password)
+          window.localStorage.setItem('UserId', JSON.stringify(res.data._id))
+          window.localStorage.setItem('IsLoggedIn', JSON.stringify(true))
+          window.location.assign('/')
+
+        } else {
+          localStorage.clear();
+          window.location.assign('/usersignup')
+        }
     })
   }
+
 
   handleSignOut = () => {
     // clear user preferences
     localStorage.clear();
+    window.location.assign('/courses')
   }
 
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Switch>
-            <Redirect exact from='/' to='/courses' />
-            <Route exact path='/courses' render={() => <Courses />} />
-            <PrivateRoute path='/courses/create' component={CreateCourse} />
-            <Route exact path='/courses/:id' component={CourseDetail} />
-            <Route exact path='/UserSignIn' render={() => <UserSignIn signIn={this.handleSignIn}/>} />
-            <PrivateRoute path='/courses/:id/update' component={UpdateCourse} />
-            <Route exact path='/UserSignUp' render={() => <UserSignUp signIn={this.handleSignIn}/> } />    
-            <Route exact path='/UserSignOut' component={UserSignOut} />     
-          </Switch>   
-        </div>
-      </BrowserRouter>
-    )
+
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Switch>
+              <Redirect exact from='/' to='/courses' />
+              <Route exact path='/courses' render={() => <Courses />} />
+              <PrivateRoute exact path='/courses/create' component={CreateCourse} />
+              <Route exact path='/courses/:id' component={CourseDetail} />
+              <Route exact path='/UserSignIn' render={() => <UserSignIn signIn={this.handleSignIn} />} />
+              <PrivateRoute exact path='/courses/:id/update' component={UpdateCourse} />
+              <Route exact path='/UserSignUp' render={() => <UserSignUp signIn={this.handleSignIn} />} />
+              <Route exact path='/UserSignOut' component={UserSignOut} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      
+    );
   }
-};
+}
 
 export default App;
